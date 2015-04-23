7ddb7c3f6cb7c2fc20873f0ccbbdd32cfe85df71
fyl00 <fyl00lee@gmail.com>
2015-04-23 10:18:44 +0800

    修复抓取精选集时歌手抓错的情况

--- ExportXiamiList.py
+++ ExportXiamiList.py
@@ -33,7 +33,8 @@ class XiamiRequest(object):
 		while True:
 			try:
 				data = self.session.get(*args, **kwargs)
-
+				for index, cookie in enumerate(self.session.cookies):
+					print ('[',index, ']',cookie)
 				if data.status_code == 403:
 					sec = re.findall('<script>document.cookie="sec=(.*?);', data.text)
 					if len(sec) != 0:
@@ -58,7 +59,7 @@ class XiamiRequest(object):
 					sec = re.findall('<script>document.cookie="sec=(.*?);', data.text)
 					if len(sec) != 0:
 						self.session.cookies['sec'] = sec[0]
-						# print ('403 ' + 'update sec=', sec[0])
+						print ('403 ' + 'update sec=', sec[0])
 						continue
 			except Exception as e:
 			# 失败重试
@@ -156,6 +157,152 @@ class XiamiLogin(XiamiRequest):
 			return
 
 
+class XiamiHandle(XiamiRequest):
+	def __init__(self,soup=None,link=None,pagecount=None,pagination=None):
+		super().__init__()
+		self.songlist = []
+		self.soup = soup
+		self.pagecount = pagecount
+		self.pagination = pagination
+	def get_u_song(self):
+		songListSoup = self.soup.find('table',class_="track_list").find_all('tr')
+		if songListSoup:
+			for song in songListSoup:
+				#check the song's checkbox
+				if 'checked="checked"' in str(song.find('td',class_="chkbox")):
+					songInfoTemp = song.find('td',class_="song_name")
+					songInfo = songInfoTemp.find_all('a')
+					songName = songInfo[0].get('title')
+
+					for i in range(1,len(songInfo)):     
+						if isRealArtist(songInfo[i]):
+							artistNameTemp = songInfo[i].get_text()
+							try :
+								artistName = artistNameTemp
+							except NameError:
+								artistName = artistName + '、' + artistNameTemp	
+
+					songLog = '正在导出第(%s/%s)页:%s - %s\n' %(self.pagination,self.pagecount,songName,artistName)
+					print_log(log,songLog)
+					self.songlist.append(artistName + " - " + songName)
+
+			return True
+
+		else:
+			return False
+
+	def get_collect_song(self):
+		songListSoup = self.soup.find(attrs={'class':'quote_song_list'}).find_all('li',class_='totle_up')
+		songSum = len(songListSoup)
+		num = 0
+		for song in songListSoup:
+			num += 1
+			#check the song's checkbox
+			if 'checked="true"' in str(song.find('span',class_='chk')):
+				songInfoTemp = song.find('span',class_='song_name')
+				songInfo = songInfoTemp.find_all('a')
+				songName = songInfo[0].get('title')
+				artistName = songInfo[1].get_text()
+
+				if len(songInfo) >= 3:
+				    for i in range(2,len(songInfo)):
+				        anotherartistName = songInfo[i].get_text()
+				        artistName = artistName + '、' +anotherartistName
+				numLog = '('+ str(num) +'/'+ str(songSum) +')'
+				songLog = '正在导出'+ numLog +':'+ songName + " - " + artistName+'\n'
+				print_log(log,songLog)
+				self.songlist.append(artistName + " - " + songName)
+
+	def link_category(self,link):
+		uLink = re.search(r'(?P<link>http:\/\/www.xiami.com\/space\/lib-song\/u\/\d+)\D*',link)
+		collectLink = re.search(r'(?P<link>http:\/\/www.xiami.com\/collect\/\d+)\D*',link)
+		if (not uLink) and (not collectLink):
+			print_log(log,'\n链接错误，请重新校对链接')
+		elif uLink:
+			url = uLink.group('link')
+			category = 'u'
+		elif collectLink:
+			url = collectLink.group('link')
+			category = 'collect'
+		return (category,url)
+
+	def create_songlist_xml(self,listname,filename,notice):
+		doc = minidom.Document()
+		doclsit = doc.createElement("List")
+		doclsit.setAttribute('ListName',listname)
+		doc.appendChild(doclsit)
+
+		for song in self.songlist:
+			fileNode = doc.createElement('File')
+			filenameNode = doc.createElement('FileName')
+			songname= song+".mp3"
+			filenameNode.appendChild(doc.createTextNode(songname))
+			fileNode.appendChild(filenameNode)
+			doclsit.appendChild(fileNode)
+
+		f = open(filename,mode='w',encoding='utf-8')
+		doc.writexml(f)
+		f.close()
+		endNotice = str(notice) + '\n已抓取的歌曲列表已保存在程序所在文件夹下的「'+ filename +'」，可以将它导入到网易云音乐了！\n'
+		print_log(log,endNotice)
+
+	def get_list(self,link):
+		URLInfo = self.link_category(link)
+		userURL = URLInfo[1]	
+		songList = []
+		self.soup = BeautifulSoup(self._safe_get(userURL,headers={'User-agent': 'Mozilla/5.0'}).content)
+		
+		
+		if URLInfo[0] == 'u':
+			xmllistname = '虾米红心'
+			xmlFileName = 'Xiami.kgl'
+			songSumSoup = self.soup.find(attrs={'class':'all_page'})
+			songSum = re.search(r'共(?P<sum>\d+)条',str(songSumSoup))
+			songSum = songSum.group('sum')
+
+			# if int(songSum) > 500 :
+			# 	print_log(log,"由于需要，正在登录虾米，请等待……")
+			# 	XiamiLogin().login_xiami('PleaseEnterUsername','PleaseEnterPassword')
+
+			self.pagecount = math.ceil(int(songSum)/25)
+			self.pagination = 1
+			self.get_u_song()
+			# get other page song
+			
+			if self.pagecount > 1 :
+				for i in range(2,self.pagecount+1):
+					self.pagination = i
+					XiamiPageURL = userURL + "/page/" + str(i)
+					try:
+						resp = self._safe_get(XiamiPageURL,
+							headers={'User-agent': 'Mozilla/5.0'})
+						self.soup = BeautifulSoup(resp.text)
+
+						isContinue = self.get_u_song()
+						if not isContinue:
+							pageLog = "=== 由于虾米网页 Bug ，从 %s 页开始已经没有歌曲 ===" %str(i)
+							print_log(log,pageLog)
+							notice = '\n******* 抓取已完成！*******'
+							self.create_songlist_xml(xmllistname,xmlFileName,notice)
+							return
+
+					except Exception as err:
+						errorCode = "\n"+str(err) + "\n虾米歌单网页打开失败，请重新校对链接或稍后再试"
+						notice = errorCode + "\n已抓取到" + str(i) + "页，下次可以从这一页开始抓取"
+						self.create_songlist_xml(xmllistname,xmlFileName,notice)
+						return
+
+		elif URLInfo[0] == 'collect':
+			filenametemp = re.search(r'\D+\/(?P<filename>\d+)',URLInfo[1])
+			xmlFileName = 'collect_'+str(filenametemp.group('filename'))+'.kgl'
+			xmllistname = self.soup.find(attrs={'class':'info_collect_main'}).h2.get_text()
+			self.get_collect_song()
+
+		notice = '\n******* 抓取已完成！*******'
+
+		self.create_songlist_xml(xmllistname,xmlFileName,notice)
+
+#login popup
 class LoginPopup(Toplevel):
 	"""docstring for LoginPopup"""
 	def __init__(self,notice,captcha,userLogin):
@@ -213,177 +360,6 @@ class LoginPopup(Toplevel):
 		self.destroy()
 
 
-class XiamiHandle(XiamiRequest):
-	def __init__(self,soup=None,link=None,pagecount=None,pagination=None):
-		super().__init__()
-		self.songlist = []
-		self.soup = soup
-		self.pagecount = pagecount
-		self.pagination = pagination
-	def get_u_song(self):
-		songListSoup = self.soup.find('table',class_="track_list").find_all('tr')
-		if songListSoup:
-			for song in songListSoup:
-				#check the song's checkbox
-				if 'checked="checked"' in str(song.find('td',class_="chkbox")):
-					songInfoTemp = song.find('td',class_="song_name")
-					songInfo = songInfoTemp.find_all('a')
-					songName = songInfo[0].get('title')
-
-					for i in range(1,len(songInfo)):     
-						if isRealArtist(songInfo[i]):
-							artistNameTemp = songInfo[i].get_text()
-							try :
-								artistName = artistNameTemp
-							except NameError:
-								artistName = artistName + '、' + artistNameTemp	
-
-					songLog = '正在导出第(%s/%s)页:%s - %s\n' %(self.pagination,self.pagecount,songName,artistName)
-					print_log(log,songLog)
-					self.songlist.append(artistName + " - " + songName)
-
-			return True
-
-		else:
-			return False
-
-	def get_collect_song(self):
-		songListSoup = self.soup.find(attrs={'class':'quote_song_list'}).find_all('li',class_='totle_up')
-		songSum = len(songListSoup)
-		num = 0
-		for song in songListSoup:
-			num += 1
-			#check the song's checkbox
-			if 'checked="true"' in str(song.find('span',class_='chk')):
-				songInfoTemp = song.find('span',class_='song_name')
-				songInfo = songInfoTemp.find_all('a')
-				songName = songInfo[0].get('title')
-				artistName = songInfo[1].get_text()
-
-				if len(songInfo) >= 3:
-				    for i in range(1,len(songInfo)):
-				        anotherartistName = songInfo[i-1].get_text()
-				        artistName = artistName + '、' +anotherartistName
-				numLog = '('+ str(num) +'/'+ str(songSum) +')'
-				songLog = '正在导出'+ numLog +':'+ songName + " - " + artistName+'\n'
-				print_log(log,songLog)
-				self.songlist.append(artistName + " - " + songName)
-
-	def link_category(self,link):
-		uLink = re.search(r'(?P<link>http:\/\/www.xiami.com\/space\/lib-song\/u\/\d+)\D*',link)
-		collectLink = re.search(r'(?P<link>http:\/\/www.xiami.com\/collect\/\d+)\D*',link)
-		if (not uLink) and (not collectLink):
-			print_log(log,'\n链接错误，请重新校对链接')
-		elif uLink:
-			url = uLink.group('link')
-			category = 'u'
-		elif collectLink:
-			url = collectLink.group('link')
-			category = 'collect'
-		return (category,url)
-
-	def create_songlist_xml(self,listname,filename,notice):
-		doc = minidom.Document()
-		doclsit = doc.createElement("List")
-		doclsit.setAttribute('ListName',listname)
-		doc.appendChild(doclsit)
-
-		for song in self.songlist:
-			fileNode = doc.createElement('File')
-			filenameNode = doc.createElement('FileName')
-			songname= song+".mp3"
-			filenameNode.appendChild(doc.createTextNode(songname))
-			fileNode.appendChild(filenameNode)
-			doclsit.appendChild(fileNode)
-
-		f = open(filename,mode='w',encoding='utf-8')
-		doc.writexml(f)
-		f.close()
-		endNotice = str(notice) + '\n已抓取的歌曲列表已保存在程序所在文件夹下的「'+ filename +'」，可以将它导入到网易云音乐了！\n'
-		print_log(log,endNotice)
-
-	def get_list(self,link):
-		URLInfo = self.link_category(link)
-		userURL = URLInfo[1]	
-		songList = []
-		self.soup = BeautifulSoup(self._safe_get(userURL,headers={'User-agent': 'Mozilla/5.0'}).content)
-		
-		
-		if URLInfo[0] == 'u':
-			xmllistname = '虾米红心'
-			xmlFileName = 'Xiami.kgl'
-			songSumSoup = self.soup.find(attrs={'class':'all_page'})
-			songSum = re.search(r'共(?P<sum>\d+)条',str(songSumSoup))
-			songSum = songSum.group('sum')
-
-			if int(songSum) > 500 :
-				print_log(log,"由于需要，正在登录虾米，请等待……")
-				XiamiLogin().login_xiami('PleaseEnterUsername','PleaseEnterPassword')
-
-			self.get_u_song()
-			# get other page song
-			self.pagecount = math.ceil(int(songSum)/25)
-			if self.pagecount > 1 :
-				for i in range(2,self.pagecount+1):
-					self.pagination = i
-					XiamiPageURL = userURL + "/page/" + str(i)
-					try:
-						resp = self._safe_get(XiamiPageURL,
-							headers={'User-agent': 'Mozilla/5.0'})
-						self.soup = BeautifulSoup(resp.text)
-
-						isContinue = self.get_u_song()
-						if not isContinue:
-							pageLog = "=== 由于虾米网页 Bug ，从 %s 页开始已经没有歌曲 ===" %str(i)
-							print_log(log,pageLog)
-							notice = '\n******* 抓取已完成！*******'
-							self.create_songlist_xml(xmllistname,xmlFileName,notice)
-							return
-
-					except Exception as err:
-						errorCode = "\n"+str(err) + "\n虾米歌单网页打开失败，请重新校对链接或稍后再试"
-						notice = errorCode + "\n已抓取到" + str(i) + "页，下次可以从这一页开始抓取"
-						self.create_songlist_xml(xmllistname,xmlFileName,notice)
-						return
-
-		elif URLInfo[0] == 'collect':
-			filenametemp = re.search(r'\D+\/(?P<filename>\d+)',URLInfo[1])
-			xmlFileName = 'collect_'+str(filenametemp.group('filename'))+'.kgl'
-			xmllistname = self.soup.find(attrs={'class':'info_collect_main'}).h2.get_text()
-			self.get_collect_song()
-
-		notice = '\n******* 抓取已完成！*******'
-
-		self.create_songlist_xml(xmllistname,xmlFileName,notice)
-
-
-
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
-
-    def __init__(self):
-        super(StoppableThread, self).__init__()
-        self._stop = threading.Event()
-
-    def stop(self):
-        self._stop.set()
-
-    def stopped(self):
-        return self._stop.isSet()
-
-# #kill the pop up thread when exit by clicking X 
-
-# def killTreading():
-# 	t = threading.enumerate()
-# 	print (t)
-# 	for i in t:
-# 		print (i)
-# 		StoppableThread().stop
-
-# 	sys.exit()
-
-
 def xiamilist():
 	xiamiReq = XiamiRequest()
 	userEntryURL = userEntryLink.get()
@@ -392,8 +368,10 @@ def xiamilist():
 
 def onclick():
 	exportThread = threading.Thread(target=xiamilist)
+	#set Daemon = True could kill threading when mainthread started it be closed
 	exportThread.setDaemon(True)
 	exportThread.start()
+
 
 
 root = Tk()
@@ -423,7 +401,6 @@ scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT,fill=Y)
 scrollbar.config(command = log.yview())
 log.config(yscrollcommand=scrollbar.set)
-# root.protocol('WM_DELETE_WINDOW',killTreading)
 root.mainloop()
 
 
