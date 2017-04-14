9affb203c90fb552de84df195e779af5b6d6efad
Frye Lee <fyl00lee@gmail.com>
2017-04-14 11:07:10 +0800

    Added source link

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ class AppWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self._enbale_source_link()
 
+    def _enbale_source_link(self):
+        link_text = "源码：<a href='https://github.com/fyl00/ExportXiamiList'>GitHub</a>"
+        self.ui.sourceLabel.setText(link_text)
+        self.ui.sourceLabel.setOpenExternalLinks(True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = AppWindow()
     window.show()
